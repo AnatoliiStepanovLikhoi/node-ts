ee3ea9eb0f8bb3953028cd211d25d2db0d49e7ca
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-09 22:48:29 +0300

    add: get/post thru decorators

--- src/person/Person.ts
+++ src/person/Person.ts
@@ -1,22 +1,33 @@
-import { JsonController, Get, Post, Body } from "routing-controllers";
+import { JsonController, Get, Post, Body, Param } from "routing-controllers";
 import { App } from "infra/app";
+import PersonService from "./PersonService";
 
-interface IPerson {
-  id: number;
-  name: string;
-}
+import { IPerson } from "./PersonType";
 
 @JsonController("/person")
 export default class Person {
   public app = new App();
+  public service = new PersonService();
 
-  @Get("/getHello")
-  async getHello() {
-    return "Hello!";
+  // @Get("/getHello")
+  // async getHello() {
+  //   return "Hello!";
+  // }
+
+  @Get()
+  async getAll() {
+    return this.service.getAll();
+  }
+
+  @Get("/:id")
+  async getOne(@Param("id") id: number) {
+    return this.service.getOne(id);
   }
 
   @Post()
   async setPerson(@Body() body: IPerson) {
-    return body;
+    await this.service.setPerson(body);
+
+    return "Person added";
   }
 }
