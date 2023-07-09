687e3fadeb59310781bc9cda162c5be7dd212fff
AnatoliiStepanovLikhoi <105781107+AnatoliiStepanovLikhoi@users.noreply.github.com>
2023-07-09 22:24:55 +0300

    add: first route

--- src/person/Person.ts
+++ src/person/Person.ts
@@ -1,5 +1,22 @@
+import { JsonController, Get, Post, Body } from "routing-controllers";
 import { App } from "infra/app";
 
+interface IPerson {
+  id: number;
+  name: string;
+}
+
+@JsonController("/person")
 export default class Person {
   public app = new App();
+
+  @Get("/getHello")
+  async getHello() {
+    return "Hello!";
+  }
+
+  @Post()
+  async setPerson(@Body() body: IPerson) {
+    return body;
+  }
 }
